5d7f4b32d27687ff1b4eeb19a2f91bb295216f94
D4nM <125446410+D4nM@users.noreply.github.com>
2023-03-27 18:40:41 +0200

    Update compile-link-run_v2.sh

--- compile-link-run_v2.sh
+++ compile-link-run_v2.sh
@@ -1,4 +1,4 @@
 set -e
 g++ SIR_model_v2.cpp -c -Wextra -Wall -fsanitize=address -fno-omit-frame-pointer
-g++ SIR_model_v2.o -lsfml-graphics -lsfml-window -lsfml-system -fsanitize=address -o Build-SIR/SIR_model_v2
-./Build-SIR/SIR_model_v2
+g++ SIR_model_v2.o -lsfml-graphics -lsfml-window -lsfml-system -fsanitize=address -o SIR_model_v2_executable
+./SIR_model_v2_executable
